47b329941fdbc64de16b27fa44a631b9a402958a
JS Morisset <jsm@surniaulula.com>
2016-10-17 09:48:13 -0700

    v1.0.6-1

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -4,10 +4,10 @@
  * Plugin Slug: wpsso-organization
  * Text Domain: wpsso-organization
  * Domain Path: /languages
- * Plugin URI: http://surniaulula.com/extend/plugins/wpsso-organization/
+ * Plugin URI: https://surniaulula.com/extend/plugins/wpsso-organization/
  * Assets URI: https://surniaulula.github.io/wpsso-organization/assets/
  * Author: JS Morisset
- * Author URI: http://surniaulula.com/
+ * Author URI: https://surniaulula.com/
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to manage multiple Organizations / Publishers and additional properties for the Schema Article types (BlogPosting, etc.).
@@ -22,7 +22,7 @@
  *	{bugfix}	Bugfixes and/or very minor improvements.
  *	{stage}{level}	dev# (development), rc# (release candidate), # (production release)
  * 
- * Copyright 2014-2016 Jean-Sebastien Morisset (http://surniaulula.com/)
+ * Copyright 2014-2016 Jean-Sebastien Morisset (https://surniaulula.com/)
  */
 
 if ( ! defined( 'ABSPATH' ) ) 
