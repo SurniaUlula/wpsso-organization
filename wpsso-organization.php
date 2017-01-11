ae4784e367b1cf231b2833da0c3c2a2c36c5685e
JS Morisset <jsm@surniaulula.com>
2017-01-11 13:09:30 -0800

    v1.0.11-1

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO extension to manage multiple Organizations / Publishers and additional properties for the Schema Article types (BlogPosting, etc.).
  * Requires At Least: 3.7
- * Tested Up To: 4.7
+ * Tested Up To: 4.7.1
  * Version: 1.0.11-1
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
