49bf9fe21c8a39f956f7130160f2136202ec9a67
JS Morisset <jsm@surniaulula.com>
2017-02-16 19:11:04 -0800

    v1.0.12-dev2

--- lib/config.php
+++ lib/config.php
@@ -28,7 +28,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.39.9-dev1',
+						'min_version' => '3.39.9-dev2',
 					),
 					'img' => array(
 						'icon_small' => 'images/icon-128x128.png',
