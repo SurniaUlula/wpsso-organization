baf16a5fceda40897fcd0010ec80eba8b69c6d61
JS Morisset <jsm@surniaulula.com>
2017-03-25 03:33:11 -0700

    v1.0.14-1

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -13,7 +13,7 @@
  * Description: WPSSO extension to manage Organizations and additional Schema Article / Event properties (Publisher, Organizer, Performer, etc.).
  * Requires At Least: 3.7
  * Tested Up To: 4.7.3
- * Version: 1.0.14-rc2
+ * Version: 1.0.14-1
  * 
  * Version Numbering Scheme: {major}.{minor}.{bugfix}-{stage}{level}
  *
