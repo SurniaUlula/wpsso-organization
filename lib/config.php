2b33d369921c801aea0b5d6b81f614c56f106c3b
JS Morisset <jsm@surniaulula.com>
2017-04-27 11:31:33 +0200

    v1.0.18-dev.2

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoorg' => array(
-					'version' => '1.0.18-dev.1',		// plugin version
+					'version' => '1.0.18-dev.2',		// plugin version
 					'opt_version' => '2',		// increment when changing default options
 					'short' => 'WPSSO ORG',		// short plugin name
 					'name' => 'WPSSO Organization Markup (WPSSO ORG)',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 					'req' => array(
 						'short' => 'WPSSO',
 						'name' => 'WordPress Social Sharing Optimization (WPSSO)',
-						'min_version' => '3.42.0-dev.1',
+						'min_version' => '3.42.0-dev.2',
 					),
 					'img' => array(
 						'icons' => array(
