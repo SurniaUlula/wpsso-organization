718716f09d1e155ed5c5c49224345469019b3b33
JS Morisset <jsm@surniaulula.com>
2017-09-30 11:05:16 -0400

    v1.1.2

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,5 +1,5 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array(s)
+// extract of translatable text strings from static config array
 _x( '[New Organization]', 'option value', 'wpsso-organization' );
 _x( '[None]', 'option value', 'wpsso-organization' );
 _x( 'Organization', 'lib file description', 'wpsso-organization' );
