d11f6d2cdb507e87150b6ac2285fc2f1ec8516c2
JS Morisset <jsm@surniaulula.com>
2018-03-08 09:26:58 -0600

    v1.2.0

--- lib/config-gettext-extract.php
+++ lib/config-gettext-extract.php
@@ -1,4 +1,4 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
 // extract of translatable text strings from static config array
 _x( 'Organization', 'lib file description', 'wpsso-organization' );
-_x( 'WPSSO Core extension to manage Organizations and additional Schema Article / Event properties (Publisher, Organizer, Performer, etc.).', 'plugin description', 'wpsso-organization' );
+_x( 'WPSSO Core extension to manage Organizations and additional Schema markup properties (Organizer, Performer, Publisher, etc.) for Google Rich Cards / Knowledge Graph.', 'plugin description', 'wpsso-organization' );
