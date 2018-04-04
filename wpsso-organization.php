e085f3707bbe97bc4a2ab4ef9950b45a5dc6b9d2
JS Morisset <jsm@surniaulula.com>
2018-04-03 22:16:49 -0400

    v1.2.2-dev.1

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on to manage Organizations and additional Schema markup properties (Organizer, Performer, Publisher, etc.) for Google Rich Cards / Knowledge Graph.
  * Requires PHP: 5.4
  * Requires At Least: 3.8
- * Tested Up To: 4.9.4
+ * Tested Up To: 4.9.5
  * Version: 1.2.2-dev.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
