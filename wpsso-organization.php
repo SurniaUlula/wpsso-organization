83c9b2a55bbb2cd9add47ae40b4d8a54dd1fed48
JS Morisset <jsm@surniaulula.com>
2018-10-24 06:12:28 -0600

    v1.4.3

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.4
  * Requires At Least: 3.8
  * Tested Up To: 5.0
- * Version: 1.4.3-b.1
+ * Version: 1.4.3
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
