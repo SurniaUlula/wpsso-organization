11e59731fa1635f2b830b5a882297b5f9b05c46d
JS Morisset <jsm@surniaulula.com>
2018-12-15 17:42:42 -0600

    v1.4.5

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -1,4 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for...' );
-// extract of translatable text strings from static config array
+
+/**
+ * Extract of translatable text strings from the static config array.
+ */
 _x( 'Organization', 'lib file description', 'wpsso-organization' );
 _x( 'WPSSO Core add-on to manage Organizations and additional Schema markup properties (Organizer, Performer, Publisher, etc.) for Google Rich Cards / Knowledge Graph.', 'plugin description', 'wpsso-organization' );
---
+++ lib/gettext-pro.php
@@ -0,0 +1,16 @@
+<?php die( 'These aren\'t the droids you\'re looking for...' );
+
+/**
+ * Extract of translatable text strings from the lib/pro classes.
+ */
+_x( 'Edit an Organization', 'option label', 'wpsso-organization' );
+_x( 'delete this organization', 'option comment', 'wpsso-organization' );
+_x( 'Organization Name', 'option label', 'wpsso-organization' );
+_x( 'Organization Alternate Name', 'option label', 'wpsso-organization' );
+_x( 'Organization Description', 'option label', 'wpsso-organization' );
+_x( 'Organization WebSite URL', 'option label', 'wpsso-organization' );
+_x( 'Organization Logo URL', 'option label', 'wpsso-organization' );
+_x( 'Organization Banner URL', 'option label', 'wpsso-organization' );
+_x( 'Organization Schema Type', 'option label', 'wpsso-organization' );
+_x( 'Organization Place / Location', 'option label', 'wpsso-organization' );
+_x( 'Google\'s Knowledge Graph', 'metabox title', 'wpsso-organization' );
