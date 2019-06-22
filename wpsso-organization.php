30197cc107f174fa7d0959fe472eead493b624bd
JS Morisset <jsm@surniaulula.com>
2019-06-22 12:06:05 -0700

    v2.0.0-b.1

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -13,7 +13,7 @@
  * Description: WPSSO Core add-on to customize the home page Schema Organization markup and manage additional Organizations (publisher, organizer, performer, etc.).
  * Requires At Least: 3.8
  * Tested Up To: 5.2.1
- * Version: 2.0.0-dev.5
+ * Version: 2.0.0-b.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
