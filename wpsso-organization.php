b00b18f46014905d18cc4636397737fef9074508
JS Morisset <jsm@surniaulula.com>
2019-06-26 06:55:51 -0700

    v2.0.0

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: WPSSO Core add-on to customize the home page Schema Organization markup and manage additional Organizations (publisher, organizer, performer, etc.).
  * Requires At Least: 3.8
- * Tested Up To: 5.2.1
+ * Tested Up To: 5.2.2
  * Version: 2.0.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
