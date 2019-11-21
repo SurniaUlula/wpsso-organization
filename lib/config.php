7ea647ee19a7aea3d5926c70714bef8ecb013f99
JS Morisset <jsm@surniaulula.com>
2019-11-21 14:11:30 -0800

    v2.1.7-dev.2

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoorg' => array(			// Plugin acronym.
-					'version'     => '2.1.6',	// Plugin version.
+					'version'     => '2.1.7-dev.2',	// Plugin version.
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO ORG',	// Short plugin name.
 					'name'        => 'WPSSO Organization Markup',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 					'req'         => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '6.12.0',
+						'min_version' => '6.13.0-dev.2',
 					),
 					'assets' => array(
 						'icons' => array(
--- lib/register.php
+++ lib/register.php
@@ -99,11 +99,14 @@ if ( ! class_exists( 'WpssoOrgRegister' ) ) {
 
 			if ( class_exists( 'Wpsso' ) ) {
 
-				if ( class_exists( 'WpssoUtil' ) ) {	// Just in case.
+				/**
+				 * Register plugin install, activation, update times.
+				 */
+				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO v6.13.0.
 
 					$version = WpssoOrgConfig::$cf[ 'plugin' ][ 'wpssoorg' ][ 'version' ];
 
-					WpssoUtil::register_ext_version( 'wpssoorg', $version );
+					WpssoUtilReg::update_ext_version( 'wpssoorg', $version );
 				}
 			}
 		}
