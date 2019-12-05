dc23fca3f6e01b3c4767bb0d2fdf23b176c024c3
JS Morisset <jsm@surniaulula.com>
2019-12-05 12:48:10 -0800

    v2.1.8-dev.2

--- wpsso-organization.php
+++ wpsso-organization.php
@@ -13,7 +13,7 @@
  * Description: Customize home page Schema Organization markup and manage additional Organizations (publisher, organizer, etc.).
  * Requires At Least: 3.9
  * Tested Up To: 5.3
- * Version: 2.1.8-dev.1
+ * Version: 2.1.8-dev.2
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
