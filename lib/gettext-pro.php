b8438a057062928afdb26a336a441bd807e3f6d7
JS Morisset <jsm@surniaulula.com>
2020-01-06 12:23:08 -0800

    v2.1.8

--- lib/gettext-pro.php
+++ lib/gettext-pro.php
@@ -12,5 +12,5 @@ _x( 'Organization WebSite URL', 'option label', 'wpsso-organization' );
 _x( 'Organization Logo URL', 'option label', 'wpsso-organization' );
 _x( 'Organization Banner URL', 'option label', 'wpsso-organization' );
 _x( 'Organization Schema Type', 'option label', 'wpsso-organization' );
-_x( 'Organization Place', 'option label', 'wpsso-organization' );
+_x( 'Organization Location', 'option label', 'wpsso-organization' );
 _x( 'Google\'s Knowledge Graph', 'metabox title', 'wpsso-organization' );
