f0926c13046ed7e75922cc30bc4265fce6b7e545
JS Morisset <jsm@surniaulula.com>
2020-01-16 05:51:57 -0800

    v2.2.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoorg' => array(			// Plugin acronym.
-					'version'     => '2.2.0-b.1',	// Plugin version.
+					'version'     => '2.2.0-rc.1',	// Plugin version.
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO ORG',	// Short plugin name.
 					'name'        => 'WPSSO Organization Markup',
