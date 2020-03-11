338894bed1bfabaa0ad74bd47a2fcc0158c1e71f
JS Morisset <jsm@surniaulula.com>
2020-03-10 18:21:48 -0700

    v2.3.0

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoorg' => array(			// Plugin acronym.
-					'version'     => '2.3.0-rc.1',	// Plugin version.
+					'version'     => '2.3.0',	// Plugin version.
 					'opt_version' => '3',		// Increment when changing default option values.
 					'short'       => 'WPSSO ORG',	// Short plugin name.
 					'name'        => 'WPSSO Organization Markup',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoOrgConfig' ) ) {
 					'req'         => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '6.24.0-rc.1',
+						'min_version' => '6.24.0',
 					),
 					'assets' => array(
 						'icons' => array(
