4282bb67f86a3810fa5aabe3b46007c0481c3dc4
JS Morisset <jsm@surniaulula.com>
2020-04-05 13:32:23 -0700

    v3.0.0-rc.1

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -4,4 +4,4 @@
  * Extract of translatable text strings from the static config array.
  */
 _x( 'Organizations', 'lib file description', 'wpsso-organization' );
-_x( 'Customize the home page Schema Organization markup and manage additional Organizations (publisher, organizer, etc.).', 'plugin description', 'wpsso-organization' );
+_x( 'Customize the Home Page Schema Organization Markup and Manage Additional Organizations (Publisher, Organizer, etc.).', 'plugin description', 'wpsso-organization' );
