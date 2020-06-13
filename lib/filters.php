9899041f25dbd037820f71a2daf00893ec486565
JS Morisset <jsm@surniaulula.com>
2020-06-13 07:16:27 -0700

    v3.3.0

--- lib/filters.php
+++ lib/filters.php
@@ -111,6 +111,9 @@ if ( ! class_exists( 'WpssoOrgFilters' ) ) {
 			return $type;
 		}
 
+		/**
+		 * $doing_upgrade added in WPSSO Core v4.4.0.
+		 */
 		public function filter_save_options( $opts, $options_name, $network, $doing_upgrade ) {
 
 			if ( $this->p->debug->enabled ) {
