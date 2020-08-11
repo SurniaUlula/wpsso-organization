c28dd50cea31d41ce2f78d2581326dfd8652778a
JS Morisset <jsm@surniaulula.com>
2020-08-11 12:50:12 -0700

    v3.7.0-rc.1

--- lib/filters.php
+++ lib/filters.php
@@ -264,7 +264,7 @@ if ( ! class_exists( 'WpssoOrgFilters' ) ) {
 
 				if ( $image_dims !== $required_dims ) {
 
-					if ( $image_dims === '-1x-1px' ) {
+					if ( '-1x-1px' === $image_dims ) {
 
 						$error_msg = sprintf( __( 'The "%s" organization banner URL image dimensions cannot be determined.',
 							'wpsso-organization' ), $org_name ) . ' ';
