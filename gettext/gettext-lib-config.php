867a0456ec4b57f89883f23aab62c10f1b37dfce
JS Morisset <jsm@surniaulula.com>
2020-09-13 12:04:59 -0700

    v3.7.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,7 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
 
 /**
- * Extract of translatable strings from the config array.
+ * Extract of translatable strings from lib/config.php.
  */
 _x( 'Organizations', 'lib file description', 'wpsso-organization' );
 _x( 'Customize the Schema Organization Markup for your WebSite and Manage Additional Organizations (Publisher, Organizer, etc.).', 'plugin description', 'wpsso-organization' );
