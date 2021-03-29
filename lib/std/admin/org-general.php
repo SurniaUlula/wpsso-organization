521dcaa096fcb9b76f195aec416288a881d73d93
JS Morisset <jsm@surniaulula.com>
2021-03-29 11:25:01 -0700

    v3.10.1

--- lib/std/admin/org-general.php
+++ lib/std/admin/org-general.php
@@ -52,53 +52,58 @@ if ( ! class_exists( 'WpssoOrgStdAdminOrgGeneral' ) ) {
 			$form->defaults[ 'org_place_id_' . $id ]    = $form->defaults[ 'site_org_place_id' ];		// Set default value.
 
 			$table_rows[ 'org_delete_' . $id ] = '' .
-			$form->get_th_html() . 
-			'<td class="blank">' . $form->get_no_checkbox( 'org_delete_' . $id ) . ' <em>' . 
-			_x( 'delete this organization', 'option comment', 'wpsso-organization' ) . '</em></td>';
+				$form->get_th_html() . 
+				'<td class="blank">' . $form->get_no_checkbox( 'org_delete_' . $id ) . ' ' . 
+				_x( 'delete this organization', 'option comment', 'wpsso-organization' ) . '</td>';
 
 			$table_rows[ 'org_name_' . $id ] = '' .
-			$form->get_th_html_locale( _x( 'Organization Name', 'option label', 'wpsso-organization' ),
-				$css_class = '', $css_id = 'org_name' ) .  
-			'<td class="blank">' . $form->get_no_input_value( '', $css_class = 'long_name is_required' ) . '</td>';
+				$form->get_th_html_locale( _x( 'Organization Name', 'option label', 'wpsso-organization' ),
+					$css_class = '', $css_id = 'org_name' ) .  
+				'<td class="blank">' . $form->get_no_input_value( '',
+					$css_class = 'long_name is_required' ) . '</td>';
 
 			$table_rows[ 'org_name_alt_' . $id ] = '' .
-			$form->get_th_html_locale( _x( 'Organization Alternate Name', 'option label', 'wpsso-organization' ),
-				$css_class = '', $css_id = 'org_name_alt' ) .  
-			'<td class="blank">' . $form->get_no_input_value( '', $css_class = 'long_name' ) . '</td>';
+				$form->get_th_html_locale( _x( 'Organization Alternate Name', 'option label', 'wpsso-organization' ),
+					$css_class = '', $css_id = 'org_name_alt' ) .  
+				'<td class="blank">' . $form->get_no_input_value( '',
+					$css_class = 'long_name' ) . '</td>';
 
 			$table_rows[ 'org_desc_' . $id ] = '' .
-			$form->get_th_html_locale( _x( 'Organization Description', 'option label', 'wpsso-organization' ),
-				$css_class = '', $css_id = 'org_desc' ) .  
-			'<td class="blank">' . $form->get_no_textarea_value() . '</td>';
+				$form->get_th_html_locale( _x( 'Organization Description', 'option label', 'wpsso-organization' ),
+					$css_class = '', $css_id = 'org_desc' ) .  
+				'<td class="blank">' . $form->get_no_textarea_value() . '</td>';
 
 			$table_rows[ 'org_url_' . $id ] = '' .
-			$form->get_th_html_locale( _x( 'Organization WebSite URL', 'option label', 'wpsso-organization' ),
-				$css_class = '', $css_id = 'org_url' ) . 
-			'<td class="blank">' . $form->get_no_input_value( '', $css_class = 'wide' ) . '</td>';
+				$form->get_th_html_locale( _x( 'Organization WebSite URL', 'option label', 'wpsso-organization' ),
+					$css_class = '', $css_id = 'org_url' ) . 
+				'<td class="blank">' . $form->get_no_input_value( '',
+					$css_class = 'wide' ) . '</td>';
 
 			$table_rows[ 'org_logo_url_' . $id ] = '' .
-			$form->get_th_html_locale( '<a href="https://developers.google.com/structured-data/customize/logos">' . 
-			_x( 'Organization Logo URL', 'option label', 'wpsso-organization' ) . '</a>',
-				$css_class = '', $css_id = 'org_logo_url' ) . 
-			'<td class="blank">' . $form->get_no_input_value( '', $css_class = 'wide is_required' ) . '</td>';
+				$form->get_th_html_locale( '<a href="https://developers.google.com/structured-data/customize/logos">' . 
+				_x( 'Organization Logo URL', 'option label', 'wpsso-organization' ) . '</a>',
+					$css_class = '', $css_id = 'org_logo_url' ) . 
+				'<td class="blank">' . $form->get_no_input_value( '',
+					$css_class = 'wide is_required' ) . '</td>';
 
 			$table_rows[ 'org_banner_url_' . $id ] = '' .
-			$form->get_th_html_locale( '<a href="https://developers.google.com/search/docs/data-types/article#logo-guidelines">' .
-			_x( 'Organization Banner URL', 'option label', 'wpsso-organization' ) . '</a>',
-				$css_class = '', $css_id = 'org_banner_url' ) . 
-			'<td class="blank">' . $form->get_no_input_value( '', $css_class = 'wide is_required' ) . '</td>';
+				$form->get_th_html_locale( '<a href="https://developers.google.com/search/docs/data-types/article#logo-guidelines">' .
+				_x( 'Organization Banner URL', 'option label', 'wpsso-organization' ) . '</a>',
+					$css_class = '', $css_id = 'org_banner_url' ) . 
+				'<td class="blank">' . $form->get_no_input_value( '',
+					$css_class = 'wide is_required' ) . '</td>';
 
 			$table_rows[ 'org_schema_type_' . $id ] = '' .
-			$form->get_th_html( _x( 'Organization Schema Type', 'option label', 'wpsso-organization' ),
-				$css_class = '', $css_id = 'org_schema_type' ) . 
-			'<td class="blank">' . $form->get_no_select( 'org_schema_type_' . $id, $org_types_select,
-				$css_class = 'schema_type' ) . '</td>';
+				$form->get_th_html( _x( 'Organization Schema Type', 'option label', 'wpsso-organization' ),
+					$css_class = '', $css_id = 'org_schema_type' ) . 
+				'<td class="blank">' . $form->get_no_select( 'org_schema_type_' . $id, $org_types_select,
+					$css_class = 'schema_type' ) . '</td>';
 
 			$table_rows[ 'org_place_id_' . $id ] = '' .
-			$form->get_th_html( _x( 'Organization Location', 'option label', 'wpsso-organization' ),
-				$css_class = '', $css_id = 'org_place_id' ) . 
-			'<td class="blank">' . $form->get_no_select( 'org_place_id_' . $id, $plm_place_names,
-				$css_class = 'long_name' ) . $plm_req_msg . '</td>';
+				$form->get_th_html( _x( 'Organization Location', 'option label', 'wpsso-organization' ),
+					$css_class = '', $css_id = 'org_place_id' ) . 
+				'<td class="blank">' . $form->get_no_select( 'org_place_id_' . $id, $plm_place_names,
+					$css_class = 'long_name' ) . $plm_req_msg . '</td>';
 
 			$table_rows[ 'subsection_google_knowledgegraph' ] = '<td colspan="2" class="subsection"><h4>' . 
 				_x( 'Google\'s Knowledge Graph', 'metabox title', 'wpsso-organization' ) . '</h4></td>';
@@ -112,9 +117,9 @@ if ( ! class_exists( 'WpssoOrgStdAdminOrgGeneral' ) ) {
 				$org_key_id = 'org_sameas_' . $key . '_' . $id;
 
 				$table_rows[ $org_key_id ] = '' .
-				$form->get_th_html_locale( _x( $label, 'option value', 'wpsso-organization' ),
-					$css_class = 'nowrap', $org_key_id ) . 
-				'<td class="blank">' . $form->get_no_input_value( '', strpos( $org_key_id, '_url' ) ? 'wide' : '' ) . '</td>';
+					$form->get_th_html_locale( _x( $label, 'option value', 'wpsso-organization' ),
+						$css_class = 'nowrap', $org_key_id ) . 
+					'<td class="blank">' . $form->get_no_input_value( '', strpos( $org_key_id, '_url' ) ? 'wide' : '' ) . '</td>';
 			}
 
 			return $table_rows;
